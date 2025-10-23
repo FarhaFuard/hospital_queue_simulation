c2d76c0ef0f824d54488a777760d9bec059893f6
FarhaFuard <mffarha14@gmail.com>
2025-10-23 17:19:22 +0530

    Update hospital_queue.py

--- hospital_queue.py
+++ hospital_queue.py
@@ -40,3 +40,33 @@ def setup(env, num_doctors=2, interval_patients=5):
         i += 1
         env.process(patient(env, f"Patient {i}", doctors))
         yield env.timeout(random.expovariate(1.0 / interval_patients))
+
+# Single Scenario Run (1)
+def run_single_scenario(num_doctors=2, interval_patients=5):
+    
+    #Run one simulation scenario and plot histogram of wait times
+    wait_times.clear()
+    random.seed(RANDOM_SEED)
+    env = simpy.Environment()
+    env.process(setup(env, num_doctors, interval_patients))
+    env.run(until=SIM_TIME)
+
+    plt.hist(wait_times, bins=10, color='skyblue', edgecolor='black')
+    plt.xlabel('Wait Time (minutes)')
+    plt.ylabel('Number of Patients')
+    plt.title(f'Patient Wait Times (Doctors={num_doctors}, Interval={interval_patients})')
+    plt.savefig(f'./results/wait_times_doctors{num_doctors}_interval{interval_patients}.png')
+    print(f"**Chart saved to ./results/wait_times_doctors{num_doctors}_interval{interval_patients}.png")
+    plt.close()
+
+
+
+
+# Multiple Scenarios Run (Visualization 2)
+def run_multiple_scenarios():
+    """Run multiple scenarios with different doctor numbers & patient intervals."""
+    scenarios = [
+        (2, 5),
+        (3, 5),
+        (2, 3)
+    ]
